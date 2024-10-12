3ca64926afae021cb798bc2bab3e6801c52604a1
Newhouse-Duc <ngoanhduc9a@gmail.com>
2024-10-12 22:23:07 +0700

    Initial commit
    change env

--- src/config/connectdb.js
+++ src/config/connectdb.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
     process.env.DATABASE_PASSWORD,
     {
         host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT || 3306,
         dialect: 'mysql'
     }
 );
